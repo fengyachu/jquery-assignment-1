4c3723171ac4622767842eef29f003b181d0a0f8
fengyachu <yfeng35@id.iit.edu>
2017-04-27 20:07:17 -0500

    Have done part 1

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,21 @@
 // Do your work in this file.
+$("<div>").addClass("view").appendTo(document.body)
+$("<div>").addClass("grass").appendTo(".view")
+$("<div>").addClass("sun").appendTo(".view")
+
+var i = 0
+while (i < 6) {
+    $("<div>").addClass("ray-" + i)
+    .appendTo(".sun");
+    i++;
+}
+
+$("<div>").addClass("mountain").appendTo(".view")
+$("<div>").addClass("mountain-top").appendTo(".mountain")
+
+var i=0
+do{
+    $("<div>").addClass("mountain-cap-" + i)
+    .appendTo(".mountain-top");
+    i++;
+}while(i < 4)
