121768419882972fe2c822b39875e2105bd0f1d3
fengyachu <yfeng35@id.iit.edu>
2017-04-27 21:11:24 -0500

    Have done part 2

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,17 @@
 // Do your work in this file.
+var i = 0;
+
+for (; ;) {
+
+    if (i > 196) break;
+    console.log(i);
+    if (i % 2 == 0) {
+            $("<div>").addClass("box type-1").appendTo(document.body);
+    } else {$("<div>").addClass("box type-2").appendTo(document.body)};
+    i++;
+}
+
+$(".box").click(function() {
+  $(this).toggleClass("clicked");
+  }
+);
